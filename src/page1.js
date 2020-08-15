27646d0365b7b510031b755bf583ac62bacbf87e
kamal Jyotwal <kamal.jyotwal891@gmail.com>
2020-08-15 22:26:30 +0530

    before adding sidenav

---
+++ src/page1.js
@@ -0,0 +1,20 @@
+// Intersection observer for nav *************************************************
+// var header = document.querySelector("nav");
+// var hero = document.querySelector(".about");
+// var options = {
+//     rootMargin: "-550px 0px 0px 0px"
+// };
+
+// var callbackfunc = (entries, heroObserver) => {
+//     entries.forEach((element) => {
+//         if (!element.isIntersecting) {
+//             header.classList.add("navInverse");
+//         } else {
+//             header.classList.remove("navInverse");
+//         }
+//     });
+// };
+
+// const heroObserver = new IntersectionObserver(callbackfunc, options);
+// heroObserver.observe(hero);
+// *******************************************************************************
