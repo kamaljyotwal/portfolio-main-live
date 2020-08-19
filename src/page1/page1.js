2b382b36f4aba627397b638f94edaa72f507eb67
kamal Jyotwal <kkjyotwal100@gmail.com>
2020-08-19 11:32:00 +0530

    before swapping cards (this is stable)
    this is the most stable version before i swap my project cards with materialize cards and from now on will delete css of my cards. roll back to this one if needed.

--- src/page1/page1.js
+++ src/page1/page1.js
@@ -1,4 +1,10 @@
-function openCity(evt, cityName) {
+// toggle of menu button
+// function toggleclass(x) {
+//     x.classList.toggle("change");
+//   }
+// end
+
+function openSection(evt, sectionname) {
     var i, x, tablinks;
     x = document.getElementsByClassName("sections");
     for (i = 0; i < x.length; i++) {
@@ -7,8 +13,8 @@ function openCity(evt, cityName) {
 
     tablinks = document.getElementsByClassName("innerlink");
     for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" backgroundRed", "");
+        tablinks[i].className = tablinks[i].className.replace(" effectOnLinkclickActive", "");
     }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " backgroundRed";
+    document.getElementById(sectionname).style.display = "block";
+    evt.currentTarget.className += " effectOnLinkclickActive";
 }
